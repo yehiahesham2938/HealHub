7f1a25c0cf600d8a729845ab659dfcd4bf487c43
haneen-elghawy <s-haneen.ghawy@zewailcity.edu.eg>
2023-12-18 07:15:42 -0800

    added 2 page one for the patient info,nurses info

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from NormalUser import NormalUser
 from administrator import administrator
 app = Flask(__name__ , static_folder='static')
 doctors_data = []
+patients_data= []
+nurses_data=[]
+
 @app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -65,6 +68,54 @@ def add_doctor():
 @app.route('/display_doctor_info', methods=['GET'])
 def display_doctor_info():
     return render_template('doctors_info.html', data=doctors_data)
+
+
+
+@app.route('/Add_Patient', methods=['POST'])
+def Add_Patient():
+    patient_data = {
+        'Name': request.form.get('patient_name'),
+        'Age': request.form.get('patient_age'),
+        'ID': request.form.get('patient_id'),
+        'Height': request.form.get('patient_height'),
+        'Weight': request.form.get('patient_weight'),
+        'Blood Type': request.form.get('patient_blood_type'),
+
+        'patient_condition': request.form.get('patient_condition'),
+
+        'Gender': request.form.get('Patient')
+
+    }
+
+    patients_data.append(patient_data)
+
+    return render_template('patient_info.html', data=patient_data)
+
+@app.route('/display_patients_info' , methods=['GET'])
+def display_patients_info():
+    data = [] 
+    return render_template('patient_info.html', data=patients_data)
+
+
+@app.route('/add_nurse', methods=['POST'])
+def add_nurse():
+    nurse_data = {
+        'Name': request.form.get('Nurse_name'),
+        'Age': request.form.get('Nurse_age'),
+        'ID': request.form.get('Nurse_id'),
+        'Salary': request.form.get('Nurse_salary'),
+        'Gender': request.form.get('Nurse_Gender')
+    }
+
+    nurses_data.append(nurse_data)
+
+    return render_template('Nurse_info.html', data=nurses_data)
+
+@app.route('/display_nurse_info', methods=['GET'])
+def display_nurse_info():
+    data=[]
+    return render_template('Nurse_info.html', data=nurses_data)
+
 
 
 if __name__ == '__main__':
